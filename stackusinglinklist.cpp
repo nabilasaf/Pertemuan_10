b5967ac0f45944e840cf30a81f1321168eb8d76f
nabilasaf <nabilasafitri861@gmail.com>
2024-05-11 10:26:11 +0700

    membuat publik kelas stack

--- stackusinglinklist.cpp
+++ stackusinglinklist.cpp
@@ -16,4 +16,9 @@ class Node{
 class Stack{
     private:
         Node* top; //pointer to the top node of the stack
+
+    public:
+        Stack() {
+            top = NULL; //Initialize the stack with a null top pointer
+        }
 }
